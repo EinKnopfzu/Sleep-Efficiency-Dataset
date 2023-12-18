4aabbe2f63f59fe103e4debe0aecb822b80b0796
WorkLapotop <mick.woerner@stundent.uni-halle.de>
2023-12-19 00:14:22 +0100

    Änderung am header und Footer

--- src/scatterplot.elm
+++ src/scatterplot.elm
@@ -20,39 +20,3 @@ import TypedSvg.Attributes.InPx exposing (cx, cy, height, r, width, x, y)
 import TypedSvg.Core exposing (Svg, text)
 import TypedSvg.Types exposing (AnchorAlignment(..), Length(..), Paint(..), Transform(..))
 
-
-car2Point : Car -> Maybe MyCar
-car2Point c =
-    Maybe.map5
-        (\myRetail myCityMpg myVehicleName mydealerCost mycarLen ->
-            MyCar
-                (toFloat myRetail)
-                (toFloat myCityMpg)
-                (myVehicleName
-                    ++ "("
-                    ++ String.fromInt myCityMpg
-                    ++ ","
-                    ++ String.fromInt myRetail
-                    ++ ")"
-                )
-                (toFloat mydealerCost)
-                (toFloat mycarLen)
-                0.0
-                0.0
-                0.0
-        )
-        c.retailPrice
-        c.cityMPG
-        (Just c.vehicleName)
-        c.dealerCost
-        c.carLen
-        |> Maybe.andThen
-            (\cX ->
-                Maybe.map3
-                    (\myXA myXB myXC ->
-                        { cX | myweight = toFloat myXA, mycarWidth = toFloat myXB, myengineSize = myXC }
-                    )
-                    c.weight
-                    c.carWidth
-                    c.engineSize
-            )
