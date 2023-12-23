7e1357136c0ef18a00e3157f77fe7b3b721fc299
WorkLapotop <mick.woerner@stundent.uni-halle.de>
2023-12-24 00:58:34 +0100

    Beschreibung für ImpactGraph hinzugefügt

--- src/ImpactGraph.elm
+++ src/ImpactGraph.elm
@@ -122,6 +122,14 @@ graph model =
             .point:hover circle { stroke: rgba(0, 0, 0, 1.0); fill: rgb(118, 214, 78); }
             .point:hover text { display: inline; }
           """ ]
+       , g [ transform [ Translate padding 0 ] ]
+            [  text_
+                [ x (0)                
+                , y ( padding * -1 )
+                , fontSize (TypedSvg.Types.px 16)
+                , textAnchor TypedSvg.Types.AnchorStart
+                ]
+                [ Html.text "Diese Graphen repräsentation soll Ihenn dabei helfen die Korrelation der Verhaltensweisen auf die Merkmale der Schlafqualität zu verstehen."]]
        , g [ transform [ Translate padding padding ] ]
             [ rect
             [ TypedSvg.Attributes.InPx.x1 (5)
